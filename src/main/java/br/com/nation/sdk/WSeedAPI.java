69b673ae496db6e833ce173bedc9652767dae92e
WizardedBR <luizotavio4102@hotmail.com>
2020-06-01 11:10:10 -0300

    A test.

--- src/main/java/br/com/nation/sdk/WSeedAPI.java
+++ src/main/java/br/com/nation/sdk/WSeedAPI.java
@@ -4,7 +4,6 @@ import br.com.nation.api.SeedAPI;
 import br.com.nation.api.nbt.NBTProvider;
 import br.com.nation.api.registry.SeedRegistry;
 import br.com.nation.api.title.TitleProvider;
-import br.com.nation.sdk.json.JSONProvider;
 import br.com.nation.sdk.nbt.WNBTProvider;
 import br.com.nation.sdk.registry.WSeedRegistry;
 import br.com.nation.sdk.title.WTitleProvider;
